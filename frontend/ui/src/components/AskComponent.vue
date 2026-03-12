260df4077dc8e9657b5111c633fe22a09399ba13
vikassrivastava18 <vikassrinitb@gmail.com>
2026-03-12 08:39:04 +0530

    Refactor code for sepearate utils, backend for ask functionality

--- frontend/ui/src/components/AskComponent.vue
+++ frontend/ui/src/components/AskComponent.vue
@@ -1,8 +1,26 @@
 <template>
     <div class="tab-pane fade" id="section2">
-        <label for="exampleTextarea" class="control-label">Ask me anything from Wistech Open books!</label>
-        <textarea class="form-control" id="exampleTextarea" rows="3"
-            placeholder="Enter your description here...">
-        </textarea>
+        <div class="container">
+            <i><label for="askInput" class="control-label">Ask me anything from Wistech Open books!</label></i>
+            <input class="form-control" id="askInput" 
+                placeholder="Enter your query here..."
+                @keyup.enter="console.log('entered something')"
+            >
+        </div>
+
     </div>
 </template>
+
+<script setup>
+
+
+</script>
+
+<style>
+#askInput {
+    position: fixed;
+    bottom: 10px;
+    border: 2px solid crimson;
+    max-width: 80vw;
+}
+</style>
