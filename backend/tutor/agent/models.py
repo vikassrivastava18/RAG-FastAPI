3d75e32d91ad300f4e354b883a4e5b081684b31c
vikassrivastava18 <vikassrinitb@gmail.com>
2026-09-05 15:54:46 +0530

    Add tables in backend using markdown syntax

--- backend/tutor/agent/models.py
+++ backend/tutor/agent/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=56)
+
+    def __str__(self) -> str:
+        return self.name
+
+# class Topic
+class Topic(models.Model):
+    title = models.CharField(max_length=128)
+    course = models.ForeignKey(Course, on_delete=models.RESTRICT)
+    summary = models.TextField()
+
+    def __str__(self) -> str:
+        return (f"({self.course.name})-{self.summary}")[:50]
+
+class SubTopic(models.Model):
+     topic = models.ForeignKey(Topic, on_delete=models.RESTRICT)
+     title = models.CharField(max_length=128)
+     summary = models.TextField()
+     content = models.JSONField(null=True, blank=True)
+
+     def __str__(self) -> str:
+        return (f"({self.topic.title})-{self.summary}")[:50]
+     
+
+
